ecfe6d3d8aefc4ff145a1a004126d628d6481919
DimOn <66843734+GitTopPoc@users.noreply.github.com>
2021-05-18 11:11:29 +0300

    new version admin

--- admin.php
+++ admin.php
@@ -36,9 +36,27 @@ if (isset($data['move_new'])) {
     $data['editOrder'] = 0;
 }
 
-if (isset($data['move_processing'])) {
+if (isset($data['move_processing_one'])) {
     $editingOrder = R::findOne('order', 'WHERE id =' . $data['editOrder']);
-    $editingOrder->status = 1;
+    $editingOrder->status = 6;
+    R::store($editingOrder);
+    $data['editOrder'] = 0;
+}
+if (isset($data['move_processing_two'])) {
+    $editingOrder = R::findOne('order', 'WHERE id =' . $data['editOrder']);
+    $editingOrder->status = 7;
+    R::store($editingOrder);
+    $data['editOrder'] = 0;
+}
+if (isset($data['move_processing_three'])) {
+    $editingOrder = R::findOne('order', 'WHERE id =' . $data['editOrder']);
+    $editingOrder->status = 8;
+    R::store($editingOrder);
+    $data['editOrder'] = 0;
+}
+if (isset($data['move_processing_four'])) {
+    $editingOrder = R::findOne('order', 'WHERE id =' . $data['editOrder']);
+    $editingOrder->status = 9;
     R::store($editingOrder);
     $data['editOrder'] = 0;
 }
@@ -56,7 +74,7 @@ if (isset($data['cancel_delete'])) {
 
 if (isset($data['order_delete'])) {
     $deleteOrder = R::findOne('order', 'WHERE id =' . $data['editOrder']);
-    $deleteOrder->status = 5;
+    $deleteOrder->status = 12;
     R::store($deleteOrder);
     $data['editOrder'] = 0;
 }
@@ -112,7 +130,7 @@ if (isset($data['order_delete'])) {
                         </ul>
                     </nav>
 
-                    <a class="btn blue-button col-lg-2 popup-link">Замовити</a>
+                    <a href="/order.php" class="btn blue-button col-lg-2 popup-link">Замовити</a>
                 </div>
 
             </div>
@@ -139,19 +157,15 @@ if (isset($data['order_delete'])) {
                 </div>
             </form>
 
-        <?php
+        <?php 
         } else {
         ?>
-            <div class="container orders-field">
-                <div class="order-filters col-lg-5 d-flex justify-content-lg-center">
-                    <form action="admin.php" method="post"><input type="hidden" value="3" name="zapros-type-all"><button type="submit" class="filter-button filter-1" name="show_all"><?php echo ' <p class="regular-text" >Всі</p> ' ?></button></form>
-                    <form action="admin.php" method="post"><input type="hidden" value="0" name="zapros-type-new"><button class="filter-button filter-2" name="show_new"><?php echo ' <p class="regular-text" >Нові</p> ' ?></button></form>
-                    <form action="admin.php" method="post"><input type="hidden" value="1" name="zapros-type-processing"><button class="filter-button filter-3" name="show_processing"><?php echo ' <p class="regular-text" >Виконуються</p> ' ?></button></form>
-                    <form action="admin.php" method="post"><input type="hidden" value="2" name="zapros-type-done"><button class="filter-button filter-4" name="show_done"><?php echo ' <p class="regular-text" >Виконані</p> ' ?></button></form>
 
-                    <?php
+
+
+<?php 
                     // Фільтр
-
+                   
                     if (isset($data['show_done'])) {
                         $zaprosType = $data['zapros-type-done'];
                         $_SESSION['zaprosType'] = $zaprosType;
@@ -168,27 +182,67 @@ if (isset($data['order_delete'])) {
                         $zaprosType = $data['zapros-type-all'];
                         $_SESSION['zaprosType'] = $zaprosType;
                     }
-                    if ($zaprosType =='') {
-                        if($_SESSION['zaprosType'] != '') {
+                    if ($zaprosType == '') {
+                        if ($_SESSION['zaprosType'] != '') {
                             $zaprosType = $_SESSION['zaprosType'];
                         }
                     }
-                    if ($zaprosType == 3 || $zaprosType == '' || $_SESSION['zapros-type'] == 3 ) {
-                        $zapros = "ORDER BY date_add DESC";
+                    if ($zaprosType == 3 || $zaprosType == '' || $_SESSION['zapros-type'] == 3) {
+                        $zapros = "WHERE status<=10 ORDER BY date_add DESC";
+                        $orders = R::findCollection('order', $zapros);
+                    } else if($zaprosType >= 6) {
+                        $zapros = "WHERE status>5 AND status<=10" . ' ORDER BY date_add DESC';
                         $orders = R::findCollection('order', $zapros);
                     } else {
                         $zapros = "WHERE status =" . $zaprosType . ' ORDER BY date_add DESC';
                         $orders = R::findCollection('order', $zapros);
                     }
-
                     ?>
+            <div class="container orders-field">
+                <div class="order-filters col-lg-7 d-flex jusify-content-between">
+                    <?php
+                    if($zaprosType == 3) {
+                        ?><form class="col-lg-3" action="admin.php" method="post"><input type="hidden" value="3" name="zapros-type-all"><button type="submit" class="col-lg-11 filter-button  filter-button-active" name="show_all"><?php echo ' <p class="regular-text" >Всі</p> ' ?></button></form> <?php
+                    }else { 
+                        ?> <form class="col-lg-3" action="admin.php" method="post"><input type="hidden" value="3" name="zapros-type-all"><button type="submit" class="col-lg-11 filter-button " name="show_all"><?php echo ' <p class="regular-text" >Всі</p> ' ?></button></form> <?php
+                    }
+                    ?>
+
+                    <?php
+                    if($zaprosType == 0) {
+                        ?><form class="col-lg-3" action="admin.php" method="post"><input type="hidden" value="0" name="zapros-type-new"><button class="col-lg-11 filter-button filter-button-active" name="show_new"><?php echo ' <p class="regular-text" >Нові</p> ' ?></button></form> <?php
+                    }else { 
+                        ?> <form class="col-lg-3" action="admin.php" method="post"><input type="hidden" value="0" name="zapros-type-new"><button class="col-lg-11 filter-button" name="show_new"><?php echo ' <p class="regular-text" >Нові</p> ' ?></button></form> <?php
+                    }
+                    ?>
+
+                    <?php
+                    if($zaprosType == 6) {
+                        ?><form class="col-lg-3" action="admin.php" method="post"><input type="hidden" value="6" name="zapros-type-processing"><button class="col-lg-11 filter-button filter-button-active" name="show_processing"><?php echo ' <p class="regular-text" >Виконуються</p> ' ?></button></form> <?php
+                    }else { 
+                        ?> <form class="col-lg-3" action="admin.php" method="post"><input type="hidden" value="6" name="zapros-type-processing"><button class="col-lg-11 filter-button" name="show_processing"><?php echo ' <p class="regular-text" >Виконуються</p> ' ?></button></form> <?php
+                    }
+                    ?>
+
+                    <?php
+                    if($zaprosType == 2) {
+                        ?><form class="col-lg-3" action="admin.php" method="post"><input type="hidden" value="2" name="zapros-type-done"><button class="col-lg-11 filter-button filter-button-active" name="show_done"><?php echo ' <p class="regular-text" >Виконані</p> ' ?></button></form> <?php
+                    }else {
+                        ?> <form class="col-lg-3" action="admin.php" method="post"><input type="hidden" value="2" name="zapros-type-done"><button class="col-lg-11 filter-button" name="show_done"><?php echo ' <p class="regular-text" >Виконані</p> ' ?></button></form> <?php
+                    }
+                    ?>
+
+
+                    
+                    
+                    
+                    
+                    
 
                 </div>
                 <div class="orders">
                     <?php
 
-
-                    
                     while ($order = $orders->next()) {
                     ?>
 
@@ -197,14 +251,91 @@ if (isset($data['order_delete'])) {
                                 <?php
                                 if ($order->status == 0) { ?>
                                     <p class="order-status-text order-status-text-new col-lg-3">НОВА</p>
+                                    
                                 <?php
                                 }
-                                if ($order->status == 1) { ?>
+                                if ($order->status == 6) { 
+                                    ?>
                                     <p class="order-status-text order-status-text-processing col-lg-7">ВИКОНУЄТЬСЯ</p>
+                                    <div class="box">
+                                        <div class="percent percent-step-one">
+                                            <svg>
+                                                <circle cx="70" cy="70" r="70"></circle>
+                                                <circle cx="70" cy="70" r="70"></circle>
+                                            </svg>
+                                            <div class="num">
+                                                <h2>50<span>%</span></h2>
+                                            </div>
+                                        </div>
+                                        <h2 class="text">Виконання верстки</h2>
+                                    </div>
+                                <?php
+                                }
+                                if ($order->status == 7) { 
+                                    ?>
+                                    <p class="order-status-text order-status-text-processing col-lg-7">ВИКОНУЄТЬСЯ</p>
+                                    <div class="box">
+                                        <div class="percent percent-step-two">
+                                            <svg>
+                                                <circle cx="70" cy="70" r="70"></circle>
+                                                <circle cx="70" cy="70" r="70"></circle>
+                                            </svg>
+                                            <div class="num">
+                                                <h2>75<span>%</span></h2>
+                                            </div>
+                                        </div>
+                                        <h2 class="text">Написання логіки</h2>
+                                    </div>
+                                <?php
+                                }
+                                if ($order->status == 8) { 
+                                    ?>
+                                    <p class="order-status-text order-status-text-processing col-lg-7">ВИКОНУЄТЬСЯ</p>
+                                    <div class="box">
+                                        <div class="percent percent-step-three">
+                                            <svg>
+                                                <circle cx="70" cy="70" r="70"></circle>
+                                                <circle cx="70" cy="70" r="70"></circle>
+                                            </svg>
+                                            <div class="num">
+                                                <h2>85<span>%</span></h2>
+                                            </div>
+                                        </div>
+                                        <h2 class="text">Тестування сайту та завантаження на хостинг</h2>
+                                    </div>
+                                <?php
+                                }
+                                if ($order->status == 9) { 
+                                    ?>
+                                    <p class="order-status-text order-status-text-processing col-lg-7">ВИКОНУЄТЬСЯ</p>
+                                    <div class="box">
+                                        <div class="percent percent-step-four">
+                                            <svg>
+                                                <circle cx="70" cy="70" r="70"></circle>
+                                                <circle cx="70" cy="70" r="70"></circle>
+                                            </svg>
+                                            <div class="num">
+                                                <h2>95<span>%</span></h2>
+                                            </div>
+                                        </div>
+                                        <h2 class="text">Передача сайта клієнту</h2>
+                                    </div>
                                 <?php
                                 }
                                 if ($order->status == 2) { ?>
                                     <p class="order-status-text order-status-text-done col-lg-6">ВИКОНАНО</p>
+                                    <div class="box">
+                                        <div class="percent percent-done">
+                                            <svg>
+                                                <circle cx="70" cy="70" r="70"></circle>
+                                                <circle cx="70" cy="70" r="70"></circle>
+                                            </svg>
+                                            <div class="num">
+                                                <h2>100<span>%</span></h2>
+                                            </div>
+                                        </div>
+                                        <h2 class="text">Виконана</h2>
+                                    </div>
                                 <?php
                                 }
 
@@ -212,7 +343,7 @@ if (isset($data['order_delete'])) {
                             </div>
 
                             <?php
-                       
+
 
                             if ($data['editOrder'] != $order->id) {
                             ?>
@@ -229,13 +360,16 @@ if (isset($data['order_delete'])) {
                             } else if (isset($data['move_order'])) {
                                 $zaprosType = $_SESSION['zaprosType'];
                             ?>
-                                <div class="editing col-lg-6">
+                                <div class="editing col-lg-8">
                                     <p class="regular-text editing-heading">Оберіть новий статус заявки</p>
-                                    <form class=" editing col-lg-8" action="admin.php" method="POST">
+                                    <form class=" editing d-flex justify-content-lg-between" action="admin.php" method="POST">
                                         <input type="hidden" value="<?php echo $order->id; ?>" name="editOrder">
 
                                         <button type="submit" class="order-move-new-button order-edit-button" name="move_new">Нова</button>
-                                        <button type="submit" class="order-move-processing-button order-edit-button" name="move_processing">Виконується</button>
+                                        <button type="submit" class="order-move-processing-button order-edit-button" name="move_processing_one">Верстка</button>
+                                        <button type="submit" class="order-move-processing-button order-edit-button" name="move_processing_two">Логіка</button>
+                                        <button type="submit" class="order-move-processing-button order-edit-button" name="move_processing_three">Перевірка</button>
+                                        <button type="submit" class="order-move-processing-button order-edit-button" name="move_processing_four">Передача</button>
                                         <button type="submit" class="order-delete-button order-edit-button" name="move_done">Виконана</button>
                                     </form>
                                 </div>
@@ -250,14 +384,14 @@ if (isset($data['order_delete'])) {
                                         <input type="hidden" value="<?php echo $order->id; ?>" name="editOrder">
                                         <button type="submit" class="order-delete-button  order-edit-button" name="order_delete">Видалити</button>
                                         <button type="submit" class="order-move-new-button  order-edit-button" name="cancel_delete">Скасувати</button>
-                                       
+
                                     </form>
                                 </div>
 
                             <?php
                             }
                             ?>
-                            
+
 
 
                             <div class="order-info">
